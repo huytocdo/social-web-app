3c1f332653a8544962000914e9b359e01a96a8e9
huytocdo <ndhuyspkt@gmail.com>
2018-09-17 17:07:27 +0700

    c3-creating new poll

--- lib/vocial_web/controllers/poll_controller.ex
+++ lib/vocial_web/controllers/poll_controller.ex
@@ -1,7 +1,27 @@
 defmodule VocialWeb.PollController do
   use VocialWeb, :controller
 
+  alias Vocial.Votes
+
   def index(conn, _params) do
-    render conn, "index.html"
+    polls = Votes.list_polls()
+
+    conn
+    |> render("index.html", polls: polls)
+  end
+
+  def new(conn, _params) do
+    poll = Votes.new_poll()
+
+    conn
+    |> render "new.html", poll: poll
+  end
+
+  def create(conn, %{"poll" => poll_params, "options" => options}) do
+    split_options = String.split(options, ",")
+    with {:ok, poll} <- Votes.create_poll_with_options(poll_params, split_options) do
+      conn
+      |> put_flash(:info, "Poll create successfully")
+      |> redirect(to: poll_path(conn, :index))
   end
 end
--- test/vocial_web/controllers/page_controller_test.exs
+++ test/vocial_web/controllers/page_controller_test.exs
@@ -2,7 +2,9 @@ defmodule VocialWeb.PageControllerTest do
   use VocialWeb.ConnCase
 
   test "GET /", %{conn: conn} do
+    #get the conn
     conn = get conn, "/"
+    #if html_response assert "Welcome to Phoenix" -> response 200
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
