1a1dc93863094fb11367f75f75f322a18bee37cf
huytocdo <ndhuyspkt@gmail.com>
2018-09-24 10:29:39 +0700

    paging data

--- lib/vocial/votes/votes.ex
+++ lib/vocial/votes/votes.ex
@@ -14,6 +14,26 @@ defmodule Vocial.Votes do
     |> Repo.preload([:options, :image, :vote_records, :messages])
   end
 
+  def list_most_recent_polls(page \\ 0, per_page \\ 25) do
+    Repo.all(
+      from p in Poll,
+      limit: ^per_page,
+      offset: ^(page * per_page),
+      order_by: [desc: p.inserted_at]
+    )
+    |> Repo.preload([:options, :image, :vote_records, :messages])
+  end
+
+  def list_most_recent_polls_with_extra(page \\ 0, per_page \\ 25) do
+    Repo.all(
+      from p in Poll,
+      limit: ^(per_page + 1),
+      offset: ^(page * per_page),
+      order_by: [desc: p.inserted_at]
+    )
+    |> Repo.preload([:options, :image, :vote_records, :messages])
+  end
+
   def new_poll do
     Poll.changeset(%Poll{}, %{})
   end
